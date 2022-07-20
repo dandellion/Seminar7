87bf552abe9aa6e3559ab44d0d653e84e9a06a30
Tasa <greatwillow@mail.ru>
2022-07-21 00:06:16 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿double [,] InitMatrix(int m, int n)
+{
+    double [,] matrix = new double[m,n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i,j] = Math.Round(randomizer.NextDouble()*100-50, 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите размер матрицы:");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+double[,] matrix = InitMatrix(m, n);
+
+Console.WriteLine($"Матрица размером {m}x{n}:");
+PrintMatrix(matrix);
